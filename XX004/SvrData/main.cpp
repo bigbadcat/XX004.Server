d5d17f504cdfb4b6096f56feff50763df999d3ed
XUXIANG-PC\XX <374591248@qq.com>
2018-07-29 10:46:06 +0800

    项初初始

---
+++ XX004/SvrData/main.cpp
@@ -0,0 +1,21 @@
+/*******************************************************
+* Copyright (c) 2018-2028, By XuXiang all rights reserved.
+*
+* FileName: main.cpp
+* Summary: 数据服程序入口。
+
+* Author: XuXiang
+* Date: 2018-07-29 09:49
+*******************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello SvrData!" << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ XX004/SvrGate/main.cpp
@@ -0,0 +1,21 @@
+/*******************************************************
+* Copyright (c) 2018-2028, By XuXiang all rights reserved.
+*
+* FileName: main.cpp
+* Summary: 网关程序入口。
+
+* Author: XuXiang
+* Date: 2018-07-29 10:30
+*******************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello SvrGate!" << endl;
+
+	system("pause");
+	return 0;
+}
