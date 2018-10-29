16b25a01730e7a8bcccc1c13a6c6dc74619f18b6
XUXIANG-PC\XX <374591248@qq.com>
2018-10-29 10:41:59 +0800

    添加线程信号类

---
+++ XX004/Common/Core/semaphore.cpp
@@ -0,0 +1,49 @@
+﻿/*******************************************************
+* Copyright (c) 2018-2088, By XuXiang all rights reserved.
+*
+* FileName: semaphore.cpp
+* Summary: 线程信号。
+*
+* Author: XuXiang
+* Date: 2018-10-27 15:54
+*******************************************************/
+
+#include "semaphore.h"
+#include <algorithm>
+#include <assert.h>
+
+namespace xxstd
+{
+	semaphore::semaphore(int init) : m_value(init)
+	{
+	}
+
+	void semaphore::wait()
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);
+		while (m_value <= 0)
+		{
+			m_cv.wait(lock);		//线程被阻塞并挂起到条件变量的线程队列中，且互斥锁(lock)被释放(重要！重要！重要！)
+		}
+	}
+
+	bool semaphore::try_wait()
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);		//调用wait函数的线程在m_cv.wait挂起时，m_mutex互斥锁已被释放
+		if (m_value <= 0)
+		{
+			return false;
+		}
+		--m_value;
+		return true;
+	}
+
+	void semaphore::post()
+	{
+		{
+			std::unique_lock<std::mutex> lock(m_mutex);
+			++m_value;
+		}
+		m_cv.notify_one();			//notify的时候需要解除互斥锁
+	}
+}
---
+++ XX004/Common/Core/semaphore.h
@@ -0,0 +1,43 @@
+﻿/*******************************************************
+* Copyright (c) 2018-2088, By XuXiang all rights reserved.
+*
+* FileName: semaphore.h
+* Summary: 线程信号。
+*
+* Author: XuXiang
+* Date: 2018-10-27 15:54
+*******************************************************/
+
+#ifndef __semaphore_h__
+#define __semaphore_h__
+
+#include <mutex>
+#include <condition_variable>
+
+namespace xxstd
+{
+	class semaphore
+	{
+	public:
+		//构造函数
+		semaphore(int init = 0);
+
+		//等待并消耗一个信号，若没有信号则线程挂起，直到信号到来
+		void wait();
+
+		//尝试等待并消耗一个信号，线程不会挂起
+		//return:true有信号 false没信号
+		bool try_wait();
+
+		//提交一个信号
+		void post();
+
+	private:
+
+		std::condition_variable m_cv;
+		std::mutex m_mutex;
+		int m_value;
+	};
+}
+
+#endif	//__semaphore_h__
