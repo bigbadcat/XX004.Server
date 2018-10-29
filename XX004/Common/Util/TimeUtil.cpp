09d6216d2d21f9c9b5bc8e4872127dcb8fdbb369
XUXIANG-PC\XX <374591248@qq.com>
2018-10-29 14:59:00 +0800

    添加时间操作接口

---
+++ XX004/Common/Util/TimeUtil.cpp
@@ -0,0 +1,37 @@
+﻿/*******************************************************
+* Copyright (c) 2018-2088, By XuXiang all rights reserved.
+*
+* FileName: TimeUtil.cpp
+* Summary: 时间相关通用操作函数。
+*
+* Author: XuXiang
+* Date: 2018-10-29 11:10
+*******************************************************/
+
+#include "TimeUtil.h"
+
+namespace XX004
+{
+	UInt64 TimeUtil::StartSecond = 0;
+
+	UInt64 TimeUtil::StartTickCount = 0;
+
+	UInt64 TimeUtil::GetCurrentMillisecond()
+	{
+		if (StartSecond == 0)
+		{
+			StartSecond = GetCurrentSecond() * 1000;
+			StartTickCount = GetTickCount();
+		}
+
+		Int64 nowtick = GetTickCount();
+		if (nowtick < StartTickCount)
+		{
+			StartSecond = GetCurrentSecond() * 1000;
+			StartTickCount = nowtick;
+		}
+		Int64 gap = nowtick - StartTickCount;
+		Int64 ret = StartSecond + gap;
+		return ret;
+	}
+}
