62157134b13d6e1f2e31d69a016a4baa2abe04d2
XUXIANG-PC\XX <374591248@qq.com>
2020-04-20 22:28:02 +0800

    添加初始速度

--- bin/Lua/Config/t_prof.lua
+++ bin/Lua/Config/t_prof.lua
@@ -1,7 +1,7 @@
 local t_prof =
 {
-	[1]={1, {"stb*100","hit*700","pry*200","pryd*500","crt*200","crtd*2000","enr*10","stbr*1"}},
-	[2]={2, {"stb*100","hit*650","pry*200","pryd*450","crt*250","crtd*2000","enr*10","stbr*1"}},
+	[1]={1, {"stb*100","hit*700","pry*200","pryd*500","crt*200","crtd*2000","enr*10","stbr*1","spd*500"}},
+	[2]={2, {"stb*100","hit*650","pry*200","pryd*450","crt*250","crtd*2000","enr*10","stbr*1","spd*500"}},
 }
 
 local keys = {"id", "init_attr"}
