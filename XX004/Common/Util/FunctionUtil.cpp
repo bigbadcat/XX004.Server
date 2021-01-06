e003bfb892bf2d6cb92b527ae7d8c3f508331f45
XUXIANG-PC\XX <374591248@qq.com>
2021-01-06 22:29:02 +0800

    调整命令循环代码

--- XX004/Common/Util/FunctionUtil.cpp
+++ XX004/Common/Util/FunctionUtil.cpp
@@ -9,6 +9,11 @@
 *******************************************************/
 
 #include "FunctionUtil.h"
+#if !defined(WIN)
+#include <unistd.h>
+#include<sys/types.h>
+#include <sys/stat.h>
+#endif
 
 namespace XX004
 {
